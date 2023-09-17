5442d72d636fb966acd8852ac4fc6e5bd102ec87
ReDo Programmer <redoprogrammer@gmail.com>
2023-09-17 16:01:23 +0700

    lay duong dan dua vao bien SERVER

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php 
-    echo '<pre>';
-    print_r($_SERVER);
-    echo '</pre>';
+   if(!empty($_SERVER['PATH_INFO'])){
+        $url = $_SERVER['PATH_INFO'];
+   }else{
+    $url ='/';
+   }
+   echo $url;
