0a74fecabb86ab3d431efb090154447c5c2d18ee
ReDo Programmer <redoprogrammer@gmail.com>
2023-09-17 21:03:25 +0700

    dinh nghia web root de chi toi thu muc public

--- app/views/__layouts/client_layout.php
+++ app/views/__layouts/client_layout.php
@@ -4,16 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="public/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo _WEB_ROOT;?>/public/assets/css/style.css">
 </head>
 <body>
     <?php
         $this->render('__Layouts/blocks/header');
     ?>
     <h1>noi dung trang chu nguoi dung</h1>
+    
     <?php
         $this->render('__Layouts/blocks/footer');
     ?>
-    <script src="public/assets/js/script.js"></script>
+    <script src="<?php echo _WEB_ROOT;?>/public/assets/js/script.js"></script>
 </body>
 </html>
--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,19 @@
 <?php
     define('__DIR_ROOT',__DIR__);
+
+    //xử lý http root
+    if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTP_HOST'] =='on'){
+        $web_root = 'https://'.$_SERVER['HTTP_HOST'];
+    }else{
+        $web_root = 'http://'.$_SERVER['HTTP_HOST'];
+    }
+  
+    $dr = strtolower(str_replace('\\','/',__DIR_ROOT));
+
+    $folder = str_replace(strtolower($_SERVER['DOCUMENT_ROOT']),'',$dr);
+    $web_root .=$folder;
+    define('_WEB_ROOT',$web_root);// đường dẫn project
+
     require_once 'configs/routes.php';
     require_once 'app/App.php';
     require_once 'core/Controller.php';// load basecontroller
