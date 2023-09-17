03b6bf606c7833f01718b5060814cae46732a855
ReDo Programmer <redoprogrammer@gmail.com>
2023-09-17 17:00:45 +0700

    kiem tra ton tai cua class va method trong controller

--- app/App.php
+++ app/App.php
@@ -5,15 +5,15 @@ class App
     function __construct()
     {
         // thiet lap mac dinh cho cac bien
-        global $routes;// goi bien toan cuc $routes trong file configs/routes.php
-        if(!empty($routes['default_controller'])){
+        global $routes; // goi bien toan cuc $routes trong file configs/routes.php
+        if (!empty($routes['default_controller'])) {
             $this->__controller = $routes['default_controller'];
         }
-        
+
         $this->__action = 'index';
         $this->__params = [];
 
-       $this->handleUrl();
+        $this->handleUrl();
     }
 
     function getUrl()
@@ -26,27 +26,35 @@ class App
         return $url;
     }
 
-    public function handleUrl(){
+    public function handleUrl()
+    {
         $url = $this->getUrl();
-        $urlArr =array_filter( explode('/',$url));// tach chuoi tu duong dan
-        $urlArr = array_values($urlArr);// dua ve dung chi so mang bat dau tu 0
+        $urlArr = array_filter(explode('/', $url)); // tach chuoi tu duong dan
+        $urlArr = array_values($urlArr); // dua ve dung chi so mang bat dau tu 0
 
         // xử lý controller
-        if(!empty($urlArr[0])){
-            $this->__controller = ucfirst($urlArr[0]);// gan controller, viet hoa chu cai dau tien
-        }else{
+        if (!empty($urlArr[0])) {
+            $this->__controller = ucfirst($urlArr[0]); // gan controller, viet hoa chu cai dau tien
+        } else {
             $this->__controller = ucfirst($this->__controller);
         }
 
-        if(file_exists('app/controllers/'.$this->__controller.'.php')){
-            require_once 'controllers/'.$this->__controller.'.php';
-            $this->__controller = new $this->__controller();// khoi tao class
-            unset($urlArr[0]);
-        }else{
+        if (file_exists('app/controllers/' . $this->__controller . '.php')) {
+            require_once 'controllers/' . $this->__controller . '.php';
+
+            //kiểm tra tồn tại của class $this->__controller
+            if (class_exists($this->__controller)) {
+                $this->__controller = new $this->__controller(); // khoi tao class
+                unset($urlArr[0]);
+            }else{
+                $this->loadError();
+            }
+
+        } else {
             $this->loadError(404);
         }
         // xử lý action
-        if(!empty($urlArr[1])){
+        if (!empty($urlArr[1])) {
             $this->__action = $urlArr[1];
             unset($urlArr[1]);
         }
@@ -55,7 +63,13 @@ class App
         $this->__params = array_values($urlArr);
 
 
-        call_user_func_array([$this->__controller,$this->__action],$this->__params);
+        //kiểm tra tồn tại của action trong controller
+        if (method_exists($this->__controller, $this->__action)) {
+            call_user_func_array([$this->__controller, $this->__action], $this->__params);
+        } else {
+            $this->loadError(404);
+        }
+
 
         // echo '<pre>';
         // print_r($this->__params);
@@ -63,7 +77,8 @@ class App
 
     }
 
-    public function loadError($name = '404'){
-            require_once 'errors/'.$name.'.php';
+    public function loadError($name = '404')
+    {
+        require_once 'errors/' . $name . '.php';
     }
 }
