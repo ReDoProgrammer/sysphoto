e415b616272488b203a1b4f17767ab23405f97b5
ReDo Programmer <redoprogrammer@gmail.com>
2023-10-02 20:23:48 +0700

    add timeline template on project detail view

--- app/views/admin/project/detail.php
+++ app/views/admin/project/detail.php
@@ -45,7 +45,7 @@
 
             </div>
             <div class="card-body">
-                <div class="table-responsive" style="min-height:300px; max-height:330px;">
+                <div class="table-responsive" style="min-height:200px; max-height:200px;">
                     <table class="table table-hover mb-0">
                         <thead>
                             <tr>
@@ -61,6 +61,125 @@
                         </thead>
                         <tbody id="tblTasksList"></tbody>
                     </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="card p-2">
+            <div class="card-heading">
+                <h4 class="p-4">Timeline</h4>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="activity" style="max-height:200px; overflow-y: auto;border: 1px solid #ccc; ">
+                            <div class="activity-box" >
+                                <ul class="activity-list">
+                                    <li>
+                                        <div class="activity-user">
+                                            <a href="profile.html" data-bs-toggle="tooltip" class="avatar"
+                                                aria-label="Lesley Grauer" data-bs-original-title="Lesley Grauer">
+                                                <img src="<?php echo _WEB_ROOT; ?>/public/assets/img/profiles/avatar-01.jpg"
+                                                    alt="User Image">
+                                            </a>
+                                        </div>
+                                        <div class="activity-content">
+                                            <div class="timeline-content">
+                                                <a href="profile.html" class="name">Lesley Grauer</a> added new task <a
+                                                    href="#">Hospital Administration</a>
+                                                <span class="time">4 mins ago</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="activity-user">
+                                            <a href="profile.html" class="avatar" data-bs-toggle="tooltip"
+                                                aria-label="Jeffery Lalor" data-bs-original-title="Jeffery Lalor">
+                                                <img src="<?php echo _WEB_ROOT; ?>/public/assets/img/profiles/avatar-16.jpg"
+                                                    alt="User Image">
+                                            </a>
+                                        </div>
+                                        <div class="activity-content">
+                                            <div class="timeline-content">
+                                                <a href="profile.html" class="name">Jeffery Lalor</a> added <a
+                                                    href="profile.html" class="name">Loren Gatlin</a> and <a
+                                                    href="profile.html" class="name">Tarah Shropshire</a> to project <a
+                                                    href="#">Patient appointment booking</a>
+                                                <span class="time">6 mins ago</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="activity-user">
+                                            <a href="profile.html" data-bs-toggle="tooltip" class="avatar"
+                                                aria-label="Catherine Manseau"
+                                                data-bs-original-title="Catherine Manseau">
+                                                <img src="<?php echo _WEB_ROOT; ?>/public/assets/img/profiles/avatar-08.jpg"
+                                                    alt="User Image">
+                                            </a>
+                                        </div>
+                                        <div class="activity-content">
+                                            <div class="timeline-content">
+                                                <a href="profile.html" class="name">Catherine Manseau</a> completed task
+                                                <a href="#">Appointment booking with payment gateway</a>
+                                                <span class="time">12 mins ago</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="activity-user">
+                                            <a href="#" data-bs-toggle="tooltip" class="avatar"
+                                                aria-label="Bernardo Galaviz" data-bs-original-title="Bernardo Galaviz">
+                                                <img src="<?php echo _WEB_ROOT; ?>/public/assets/img/profiles/avatar-13.jpg"
+                                                    alt="User Image">
+                                            </a>
+                                        </div>
+                                        <div class="activity-content">
+                                            <div class="timeline-content">
+                                                <a href="profile.html" class="name">Bernardo Galaviz</a> changed the
+                                                task name <a href="#">Doctor available module</a>
+                                                <span class="time">1 day ago</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="activity-user">
+                                            <a href="profile.html" data-bs-toggle="tooltip" class="avatar"
+                                                aria-label="Mike Litorus" data-bs-original-title="Mike Litorus">
+                                                <img src="<?php echo _WEB_ROOT; ?>/public/assets/img/profiles/avatar-05.jpg"
+                                                    alt="User Image">
+                                            </a>
+                                        </div>
+                                        <div class="activity-content">
+                                            <div class="timeline-content">
+                                                <a href="profile.html" class="name">Mike Litorus</a> added new task <a
+                                                    href="#">Patient and Doctor video conferencing</a>
+                                                <span class="time">2 days ago</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="activity-user">
+                                            <a href="profile.html" data-bs-toggle="tooltip" class="avatar"
+                                                aria-label="Jeffery Lalor" data-bs-original-title="Jeffery Lalor">
+                                                <img src="<?php echo _WEB_ROOT; ?>/public/assets/img/profiles/avatar-16.jpg"
+                                                    alt="User Image">
+                                            </a>
+                                        </div>
+                                        <div class="activity-content">
+                                            <div class="timeline-content">
+                                                <a href="profile.html" class="name">Jeffery Lalor</a> added <a
+                                                    href="profile.html" class="name">Jeffrey Warden</a> and <a
+                                                    href="profile.html" class="name">Bernardo Galaviz</a> to the task of
+                                                <a href="#">Private chat module</a>
+                                                <span class="time">7 days ago</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -179,6 +298,6 @@
 
 
 <?php
-     $this->render('admin/task/modal');
-    include_once 'view.php';
+$this->render('admin/task/modal');
+include_once 'view.php';
 ?>
