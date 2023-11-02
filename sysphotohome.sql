3c5424018d205a6f911bcf90f68bfe414b9f88be
redoprogrammer <redoprogrammer@gmail.com>
2023-11-02 09:01:44 +0700

    fix trigger after_task_inserted

--- sysphotohome.sql
+++ sysphotohome.sql
@@ -2160,18 +2160,23 @@ DELIMITER $$
 CREATE TRIGGER `after_task_inserted` AFTER INSERT ON `tasks` FOR EACH ROW BEGIN
 	DECLARE v_created_by varchar(100);
     DECLARE v_level varchar(50);
+    DECLARE v_level_color varchar(200);
     DECLARE v_action varchar(5000);
     DECLARE v_role varchar(100) DEFAULT '';
     
     SET v_created_by = (SELECT acronym FROM users WHERE id = (SELECT created_by FROM tasks WHERE id = NEW.id));
     SET v_level = (SELECT name FROM levels WHERE id = NEW.level_id);
+    SET v_level_color = (SELECT color FROM levels WHERE id = NEW.level_id);
+    
     SET v_role = (SELECT name FROM user_types WHERE id = (SELECT type_id FROM users WHERE id = NEW.created_by));
     
     SET v_action = CONCAT(v_role,' [<span class="fw-bold text-info">',v_created_by,'</span>] ');
-    IF NEW.cc_id > 0 THEN
-    	SET v_action = CONCAT( v_action,'<span class="text-success">INSERT CC TASK</span> [<span class="fw-bold">',v_level,'</span>] with quantity: [',NEW.quantity,']');
+    IF NEW.auto_gen = 1 THEN 
+    	SET v_action = CONCAT( v_action,'<span class="text-info">CREATE NEW TASK</span> [<span class="fw-bold ',v_level_color,'">',v_level,'</span>] FROM TEMPLATE with quantity: [',NEW.quantity,']');
+    ELSEIF NEW.cc_id > 0 THEN
+    	SET v_action = CONCAT( v_action,'<span class="text-success">INSERT NEW CC TASK</span> [<span class="fw-bold ',v_level_color,'">',v_level,'</span>] with quantity: [',NEW.quantity,']');
     ELSE
-    	SET v_action = CONCAT(v_action,'<span class="text-success">INSERT TASK</span> [<span class="fw-bold">',v_level,'</span>] with quantity: [',NEW.quantity,']');
+    	SET v_action = CONCAT(v_action,'<span class="text-success">INSERT NEW TASK</span> [<span class="fw-bold ',v_level_color,'">',v_level,'</span>] with quantity: [',NEW.quantity,']');
     END IF;
     
     INSERT INTO project_logs(project_id,task_id,timestamp,action)
