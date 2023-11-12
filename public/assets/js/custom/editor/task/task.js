abf463b7386cf70dbae7902be877271c6181acfc
ReDoProgrammer <redoprogrammer@gmail.com>
2023-11-12 18:21:25 +0700

    show task detail in editor zone

--- public/assets/js/custom/editor/task/task.js
+++ public/assets/js/custom/editor/task/task.js
@@ -33,19 +33,19 @@ $('#btnSearch').click(function (e) {
     e.preventDefault();
     LoadOwnTasks();
 })
-$('#btnSubmitTask').click(function(){
+$('#btnSubmitTask').click(function () {
     let content = $('#txtUrl').val();
-    let read_instructions = $('#ckbReadInstruction').is(':checked')?1:0;
+    let read_instructions = $('#ckbReadInstruction').is(':checked') ? 1 : 0;
 
     $.ajax({
-        url:'task/submit',
-        type:'post',
-        data:{
-                id:taskId,
-                read_instructions,
-                content
+        url: 'task/submit',
+        type: 'post',
+        data: {
+            id: taskId,
+            read_instructions,
+            content
         },
-        success:function(data){
+        success: function (data) {
             try {
                 let content = $.parseJSON(data);
                 $.toast({
@@ -54,20 +54,20 @@ $('#btnSubmitTask').click(function(){
                     showHideTransition: 'fade',
                     icon: content.icon
                 })
-                if(content.code == 200){
+                if (content.code == 200) {
                     $("#task_submit_modal").modal('hide');
                     LoadOwnTasks();
                 }
             } catch (error) {
-                console.log(data,error);
+                console.log(data, error);
             }
         }
     })
 })
 
-$('#ckbReadInstruction').on('change', function() {
+$('#ckbReadInstruction').on('change', function () {
     $('#btnSubmitTask').prop('disabled', !this.checked);
-  });
+});
 
 $("#task_submit_modal").on('shown.bs.modal', function () {
     $('#btnSubmitTask').prop('disabled', true);
@@ -76,7 +76,7 @@ $("#task_submit_modal").on('shown.bs.modal', function () {
 });
 
 $("#task_submit_modal").on("hidden.bs.modal", function () {
-   taskId = 0;
+    taskId = 0;
 });
 
 
@@ -88,7 +88,6 @@ function ViewTaskDetail(id) {
         success: function (data) {
             try {
                 let content = $.parseJSON(data);
-
                 let t = content.task;
 
                 $('#level').addClass(t.level_color);
@@ -99,60 +98,61 @@ function ViewTaskDetail(id) {
                 $('#status').addClass(t.status_color);
                 $('#status').text(t.status ? t.status : '-');
 
-                
+
 
                 $('#start_date').html(`<span class="text-danger fw-bold">${(t.start_date.split(' ')[1])}</span> <br/>${(t.start_date.split(' ')[0])}`);
                 $('#end_date').html(`<span class="text-danger fw-bold">${(t.end_date.split(' ')[1])}</span> <br/>${(t.end_date.split(' ')[0])}`);
 
                 $('#divTaskDescription').empty();
-                let s = $.parseJSON(t.styles);
-                console.log(s);
-                $('#divTaskDescription').append(`
-                    <div class="row">
-                        <div class="col-sm-4">Color mode: <span class="fw-bold">${s.color?s.color:''}</span></div>
-                        <div class="col-sm-4">Output: <span class="fw-bold">${s.output?s.output:''}</span></div>
-                        <div class="col-sm-4">Size: <span class="fw-bold">${s.size}</span></div>
-                    </div>
-                `);
-
-                $('#divTaskDescription').append(`<hr>
-                    <div class="row mt-3">
-                        <div class="col-sm-4">National style: <span class="fw-bold">${s.style?s.style:''}</span></div>
-                        <div class="col-sm-4">Cloud: <span class="fw-bold">${s.cloud?s.cloud:''}</span></div>
-                        <div class="col-sm-4">TV: <span class="fw-bold">${s.tv}</span></div>                        
-                    </div>
-                `);
-
-
-                $('#divTaskDescription').append(`<hr>
-                    <div class="row mt-3">                        
-                        <div class="col-sm-4">Sky: <span class="fw-bold">${s.sky}</span></div>
-                        <div class="col-sm-4">Fire: <span class="fw-bold">${s.fire}</span></div>
-                        <div class="col-sm-4">Grass: <span class="fw-bold">${s.grass}</span></div>
-                    </div>
-                `);
-                $('#divTaskDescription').append(`<hr>
-                    <div class="row mt-3">
-                        <div class="col-sm-12">Straighten: ${s.is_straighten==1?'<i class="fa-regular fa-square-check"></i>':'<i class="fa-regular fa-square"></i>'}
-                        <span class="fw-bold">${s.straighten_remark}</span></div>
-                    </div>
-                `)
-                $('#divTaskDescription').append(`<hr><span class="text-secondary">Style remark:</span><p class="mt-3 mb-5">${s.style_remark}</p>`);
-
 
                 if (t.cc_id > 0) {
-                    $('#divTaskDescription').append(`<span class="text-secondary">CC description:</span>`);
-                    $('#divTaskDescription').append(`<p class="mt-2">${t.cc_content}</p>`)
+                    $('#divTaskDescription').append(`<span class="text-secondary fw-bold">CC description:</span>`);
+                    $('#divTaskDescription').append(`<div class="mt-2" style="padding-left:20px;">${t.cc_content}</div>`)
                 }
 
-                $('#divTaskDescription').append(`<span class="text-secondary">Task description:</span>`);
-                $('#divTaskDescription').append(`<p class="mt-2">${t.task_description}</p>`);
+                $('#divTaskDescription').append(`<span class="text-secondary fw-bold">Task description:</span>`);
+                $('#divTaskDescription').append(`<div class="mt-2" style="padding-left:20px;">${t.task_description}</div>`);
 
                 let instructions = $.parseJSON(t.instructions_list);
-                $('#divTaskDescription').append(`<span class="text-secondary">Instructions:</span>`);
+                $('#divTaskDescription').append(`<span class="text-secondary fw-bold">Instructions:</span>`);
                 instructions.forEach(i => {
-                    $('#divTaskDescription').append(`<p class="mt-2" style="padding-left:20px;">${i.content}</p> <hr>`)
+                    $('#divTaskDescription').append(`<div class="mt-2" style="padding-left:20px;">${i.content}</div>`)
                 })
+
+                let styles = `
+                    <div class="card mb-3 bg-light">
+                        <div class="card-header bg-light fw-bold pt-3"> Customer styles</div>
+                        <div class="card-body"><hr/>`;
+                         let s = $.parseJSON(t.styles);                     
+                        styles+=    `<div class="row">
+                                        <div class="col-sm-4">Color mode: <span class="fw-bold">${s.color ? s.color : ''}</span></div>
+                                        <div class="col-sm-4">Output: <span class="fw-bold">${s.output ? s.output : ''}</span></div>
+                                        <div class="col-sm-4">Size: <span class="fw-bold">${s.size}</span></div>
+                                    </div>
+                                    <div class="row mt-3">
+                                        <div class="col-sm-4">National style: <span class="fw-bold">${s.style ? s.style : ''}</span></div>
+                                        <div class="col-sm-4">Cloud: <span class="fw-bold">${s.cloud ? s.cloud : ''}</span></div>
+                                        <div class="col-sm-4">TV: <span class="fw-bold">${s.tv}</span></div>                        
+                                    </div>
+                                    <div class="row mt-3">                        
+                                        <div class="col-sm-4">Sky: <span class="fw-bold">${s.sky}</span></div>
+                                        <div class="col-sm-4">Fire: <span class="fw-bold">${s.fire}</span></div>
+                                        <div class="col-sm-4">Grass: <span class="fw-bold">${s.grass}</span></div>
+                                    </div>
+                                    <div class="row mt-3">
+                                        <div class="col-sm-12">Straighten: ${s.is_straighten == 1 ? '<i class="fa-regular fa-square-check"></i>' : '<i class="fa-regular fa-square"></i>'}
+                                        <span class="fw-bold">${s.straighten_remark}</span></div>
+                                    </div>
+                                    `;
+                        
+                    styles+=     `</div>
+                    </div>
+                `;
+                $('#divTaskDescription').append(styles);
+
+             
+
+               
 
                 let logs = $.parseJSON(t.task_logs);
                 $('#ulTaskLogs').empty();
@@ -172,7 +172,6 @@ function ViewTaskDetail(id) {
                 $('#editor').text(t.editor ? t.editor : '-');
                 $('#qa').text(t.qa ? t.qa : '-');
                 $('#dc').text(t.dc ? t.dc : '-');
-
                 $('#task_detail_modal').modal('show');
             } catch (error) {
                 console.log(data, error);
@@ -224,6 +223,7 @@ function LoadOwnTasks() {
         },
         success: function (data) {
             try {
+
                 let content = JSON.parse(data);
                 let tasks = content.tasks;
                 let idx = (page - 1) * limit;
@@ -235,7 +235,9 @@ function LoadOwnTasks() {
                             <td><span class="fw-bold ${t.level_color}">${t.level}</span> ${t.cc_id > 0 ? '<i class="fa-regular fa-closed-captioning text-danger"></i>' : ''}</td>
                             <td><span class="text-danger fw-bold">${(t.start_date.split(' '))[1]}</span> <br/>${(t.start_date.split(' '))[0]}</td>
                             <td><span class="text-danger fw-bold">${(t.end_date.split(' '))[1]}</span> <br/>${(t.end_date.split(' '))[0]}</td>
-                            <td><span class="text-danger fw-bold">${(t.commencement_date.split(' '))[1]}</span> <br/>${(t.commencement_date.split(' '))[0]}</td>
+                            <td> <span class="text-danger fw-bold">
+                                ${t.commencement_date ? `${(t.commencement_date.split(' '))[1]}</span> <br/>${(t.commencement_date.split(' '))[0]}` : ``}
+                            </td>
                             <td class="text-center">${t.quantity}</td>
                             <td><span class="${t.status_color}">${t.status ? t.status : '-'}</span></td>
                             <td class="text-center">${t.editor ? t.editor : '-'}</td>
